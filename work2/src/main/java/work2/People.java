dd5006dabfab9d9d7d47704a6c4f7dd7a64f9d31
Matheus Kildere <matheuskildery94@gmail.com>
2019-06-21 19:21:52 -0300

    feat: add files

---
+++ work2/src/main/java/work2/People.java
@@ -0,0 +1,32 @@
+package work2;
+
+public class People {
+    private String name;
+    private String email;
+    private long cpf;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public long getCpf() {
+        return this.cpf;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+}
